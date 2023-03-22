3e3740f995925390ea7b5dd85f5d3428dd611628
Mohit <mohit1903108@gmail.com>
2023-03-22 18:28:12 +0530

    GUI Added + logic on the way

--- main.py
+++ main.py
@@ -1,54 +1,73 @@
-import socket
-import os
-import tkinter as tk
-from tkinter import filedialog
-class FileBhejo:
-    name="FileBhejo"
-    owner="Mohit Kumar Chaniyal"
-    def __init__(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        
-        host = socket.gethostbyname(socket.gethostname())
-        port = 12345       
-        self.local_address=(host,port)
-        print(self.local_address)
-    def send_file(self,remote_address):
-        root = tk.Tk()
-        root.withdraw()
-        file_path = filedialog.askopenfilename()
-        filename=file_path.split('/')[-1]
-        self.s.connect(remote_address)
-        
-        filesize=os.path.getsize(file_path)
-        file=open(file_path,"rb")
-        self.s.sendall(filename.encode())
-        self.s.sendall(str(filesize).encode())
-        chunk=file.read(4096)
-        while chunk:
-            self.s.sendall(chunk)
-            chunk=file.read(4096)
-        file.close()
-        self.s.close()
-    def receive_file(self):
-        self.s.bind(self.local_address)
-        self.s.listen(1)
+from kivy.app import App
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import ScreenManager,Screen
+import socket  
+import time
+class HomeScreen(Screen):
+    def on_kv_post(self,_):
+        self.set_ip()
+    def set_ip(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            self.s.connect(('10.255.255.255', 1))  # Try to connect to a public IP address
+            self.local_ip_address = self.s.getsockname()[0]
+        except:
+            self.local_ip_address = socket.gethostbyname(socket.gethostname())
+
+        self.port = 8888
+        def_ip="192.168.0.1"
+        if not self.local_ip_address.startswith("192.168"): 
+            self.local_ip_address=def_ip
+            self.ids.IP.text=f"IP Address:{self.local_ip_address}"
+        else:
+            self.ids.IP.text=f"IP Address:{self.local_ip_address}"
+        self.ids.port.text=f"Port Address: {self.port}"
+
+class FileChooserScreen(Screen):
+    def selected(self,filepath):
+        self.filepath=filepath
+class RecieveScreen(Screen):
+    def on_pre_enter(self, *args):
+        sm=self.manager
+        home_screen=sm.get_screen("home_screen")
+        self.s=socket.socket()
+        address=(home_screen.local_ip_address,home_screen.port)
+        self.ids.status.text="Binding"
+        self.s.bind(address)
+        self.s.listen()
+        self.ids.status.text="Wating For Conection"
+        self.ids.address.text=f"{address}"
         conn,add=self.s.accept()
-        print("Connected to:",add)
-        filename=conn.recv(4096).decode()
-        filesize=int(conn.recv(4096).decode())
-        file=open(filename,"wb")
-        recieved=0
-        while recieved<filesize:
-            chunk=conn.recv(4096)
-            file.write(chunk)
-            recieved+=len(chunk)
-        file.close()
-        conn.close()
-app=FileBhejo()
-while True :
-    ask=input("S for send R for recieve")
-    if ask.lower()=="s":
-        remote_address=input("Enter IP"),12345
-        app.send_file(remote_address)
-    elif ask.lower()=="r":
-        app.receive_file()
+        print("Connected")
+    def cancel(self):
+        self.s.close()
+class GetIPScreen(Screen):
+    def on_pre_enter(self, *args):
+        sm=self.manager
+        
+        self.home_screen=sm.get_screen("home_screen") 
+        self.ids.address.text=f"{self.home_screen.local_ip_address,self.home_screen.port}"
+    def connect(self):
+        self.s=socket.socket()
+        
+        self.s.connect((self.ids.remote_ip,self.home_screen.port))
+        self.ids.image.source="connected.png"
+        self.ids.connect.text="connected"
+        self.ids.connect.desabled=True
+        self.ids.select_file=False
+        
+
+
+class SendScreen(Screen):
+    def on_pre_enter(self, *args):
+        sm=self.manager
+        home_screen=sm.get_screen("home_screen")   
+        file_chooser_screen=sm.get("file_chooser_screen")
+
+class WindowManager(ScreenManager):
+    pass
+class FileBhejo(App):
+    def build(self):
+        return Builder.load_file("FileBhejo.kv")
+    
+FileBhejo().run()
